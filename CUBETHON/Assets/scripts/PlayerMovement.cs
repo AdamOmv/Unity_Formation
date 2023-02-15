a8bdbaa4a66e2e30428c335eaf5948623de0ec7a
ADAM <oumarov.adam@gmail.com>
2023-02-15 15:41:55 +0100

    maj CUBETHON

--- CUBETHON/Assets/scripts/PlayerMovement.cs
+++ CUBETHON/Assets/scripts/PlayerMovement.cs
@@ -5,14 +5,17 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
 
+    public PlayerMovement movement;
+
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
-    public float saut = 0.24f;
+    public float saut = 500f;
     public float descente = 0.24f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
         rb.AddForce(0,0,forwardForce * Time.deltaTime);
 
         if(Input.GetKey("d") || Input.GetKey("right"))
@@ -23,14 +26,27 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(-sidewaysForce* Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
-        if(Input.GetKey("space") && rb.position.y < 3f)
+        if(Input.GetKeyDown(KeyCode.Space) && rb.position.y < 3f)
         {
             rb.AddForce(0, saut, 0, ForceMode.VelocityChange);
-                
+            //Debug.Log("saut");  
         }
 
         if(rb.position.y < -1f)
         {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame(); 
+        }
+
+        if(rb.position.x < -8.5f)
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+        if(rb.position.x > 8.5f)
+        {
+            movement.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
         }
 
